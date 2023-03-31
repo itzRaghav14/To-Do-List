ae727c2cdb21490e4b91ffd3727be7520a4afdeb
Raghav Goel <raghavgoel1453@gmail.com>
2023-03-31 12:44:00 +0530

    First commit: create a directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,18 @@
+// setting up express app
+const express = require('express');
+const app = express();
+
+// including path
+const path = require('path');
+
+// declaring port
+const port = 8000;
+
+// app listening
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server : ${err}`);
+        return;
+    }
+    console.log(`The server is up and running at ${port}`);
+});
